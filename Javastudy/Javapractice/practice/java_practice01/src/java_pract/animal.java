62e1fa30864e672a27c4174b4c2bd3a738e85cc9
Song-Edust <827940676@qq.com>
2024-01-11 17:22:31 +0800

    qqq

---
+++ Javastudy/Javapractice/practice/java_practice01/src/java_pract/animal.java
@@ -0,0 +1,10 @@
+package java_pract;
+
+public class animal {
+    public void eat() {
+        System.out.println("eat");
+    }
+    public void drink(){
+        System.out.println("drink");
+    }
+}
